99954ef29e699e6e6d6a8878ac247a893d6d0be2
Badaruddin-MI <vrufurdy69@gmail.com>
2019-03-19 20:21:57 +0700

    modul 2 Artisan, Migration, Model, Seeder

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,28 @@
 |
 */
 
-Route::get('/', function () {
-    return view('badar');
+Route::get('/welcome', function () {
+    return view('welcome');
 });
 
-/*Route::get('/badar', function () {
-    return view('badar');
+/*Route::get('/index', function () {
+	echo "Uji Coba route dengan method GET";
 });*/
 
+Route::get('/show/{id}', function ($id) {
+	echo "Nilai Parameter Adalah ".$id;
+});
+
+Route::get('/edit/{nama}', function ($nama) {
+	echo "Nilai Parameter Adalah ".$nama;
+})->where('nama','[A-Za-z]+');
+
+Route::get('/index', function () {
+	echo "<a href='".route('create')."'>Akses Route dengan nama </a>";
+});
+
+Route::get('/create', function () {
+	echo "Route diakses menggunakan nama";
+})->name('create');
+
+Route::get('/produk', 'produkController@index');
