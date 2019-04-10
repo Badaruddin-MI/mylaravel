b60fe94b874279109a1c9fd9dec9c20604204d29
Badaruddin-MI <vrufurdy69@gmail.com>
2019-04-10 13:54:57 +0700

    modul-5 controller

--- resources/views/produk/index.blade.php
+++ resources/views/produk/index.blade.php
@@ -1,9 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Laravel Saya</title>
+	<title>Data Produk</title>
 </head>
 <body>
-		<h1>Produk : {{ isset($produk) ? $produk : 'produk kosong' }}</h1>
+	<table border="1">
+		<thead>
+			<tr>
+				<th>No</th>
+				<th>Nama Produk</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach ($produk as $i => $v)
+			<tr>
+				<td>{{ $i+1 }}</td>
+				<td>{{ $v }}</td>
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
+
 </body>
 </html>
